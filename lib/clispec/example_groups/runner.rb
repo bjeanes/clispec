2c7ad0cb4c1746d8815f793a689c0281b2f9b3c8
Bodaniel Jeanes <me@bjeanes.com>
2009-09-06 11:59:53 +1000

    Make options hash optional

--- lib/clispec/example_groups/runner.rb
+++ lib/clispec/example_groups/runner.rb
@@ -8,24 +8,24 @@ class RunnerExampleGroup < Spec::Example::ExampleGroup
     end
 
 
-    def self.it_exits(message, expected_status_code, options)
+    def self.it_exits(message, expected_status_code, options = [])
       it "exits #{message}" do
         Kernel.should_receive(:exit).with(expected_status_code)
         run(options[:when_run_with] || '')
       end
     end
 
-    def self.it_exits_with_error_code(options)
+    def self.it_exits_with_error_code(options = {})
       expected_status_code = options[:error_code] || 1
       it_exits("with error code #{expected_status_code}", expected_status_code, options)
     end
 
-    def self.it_exits_successfully(options)
+    def self.it_exits_successfully(options = {})
       expected_status_code = options[:error_code] || 0
       it_exits("without error (#{expected_status_code})", expected_status_code, options)
     end
 
-    def run(args)
+    def run(args = [])
       args = args.is_a?(String) ? args.split(' ') : args
       self.class.described_class.run(args, out_stream, error_stream)
     end
